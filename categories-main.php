5a158db608c672304aa7dfb096fc270e2da65e03
argahv <arg.ahv7@gmail.com>
2018-10-08 15:24:15 +0530

    categories

---
+++ categories-main.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Debate-Ongoing</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/index.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/footer.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/header.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/aside-online.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/categories.css" /> 
+    <link rel="stylesheet" type="text/css" media="screen" href="./css/trending-hashtags.css" /> 
+    <script src="./jquery/jquery.js"></script>
+    <script src="./jquery/jquery-ui.js"></script>
+    <script  src="./jquery/js-files.js"></script>
+    
+</head>
+<body>
+
+        <?php include('php/header.php'); ?>
+        <?php include('php/top-trending.php'); ?>
+        <?php include('php/categories.php'); ?>
+        <?php include('php/footer.php'); ?>
+
+</body>
+</html>
+
---
+++ php/categories.php
@@ -0,0 +1,19 @@
+<?php for($j=0;$j<10;$j++){ ?>
+<div class="categories--debates-main_div">
+
+    <div class="categories-contents">
+            <h2 class="category-title">Football</h2>
+            <div class="categories-img-div">
+                <img src="./images/icons8_Soccer_Ball_52px_1.png">
+            </div>
+            <p>Total no. of debates</p>
+            <p class="category--debate-number">20000</p>
+            <div class="categories--users-img-div">
+                <?php for($i=0;$i<8;$i++){ ?>
+                    <img  class="categories--users-img"src="./images/user-icon-temp.png">
+                <?php } ?>
+    </div>
+    </div>
+    
+</div>
+<?php } ?>
